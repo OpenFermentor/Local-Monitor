4e20f582028e1c7144806bbfa699261fad90c6ee
Mauricio Cousillas <cousillas806@gmail.com>
2017-08-29 18:46:40 -0300

    Update reading_view.ex

--- web/views/reading_view.ex
+++ web/views/reading_view.ex
@@ -15,7 +15,7 @@ defmodule BioMonitor.ReadingView do
       ph: reading.ph,
       co2: reading.co2,
       density: reading.density,
-      inserted_at: to_string(reading.inserted_at),
+      inserted_at: reading.inserted_at,
       routine_id: reading.routine_id}
   end
 end
