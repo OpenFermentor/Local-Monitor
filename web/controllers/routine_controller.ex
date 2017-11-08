b66566656458d1be28673b7826af0789d9d25853
mcousillas6 <cousillas806@gmail.com>
2017-11-08 11:34:16 -0300

    Removed density from csv

--- web/controllers/routine_controller.ex
+++ web/controllers/routine_controller.ex
@@ -117,7 +117,7 @@ defmodule BioMonitor.RoutineController do
     file = File.open!(Path.expand(path), [:write, :utf8])
 
     routine.readings
-      |> CSV.encode(headers: [:temp, :ph, :density, :inserted_at])
+      |> CSV.encode(headers: [:temp, :ph, :inserted_at])
       |> Enum.each(&IO.write(file, &1))
 
     conn = conn
