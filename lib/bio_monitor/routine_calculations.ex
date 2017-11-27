d271bdcaedf1faf86df3d4d06f073eb3fb1d9d47
mcousillas6 <cousillas806@gmail.com>
2017-11-27 17:48:17 -0300

    Fixed q calculations

--- lib/bio_monitor/routine_calculations.ex
+++ lib/bio_monitor/routine_calculations.ex
@@ -11,26 +11,32 @@ defmodule BioMonitor.RoutineCalculations do
     defstruct x: 0, y: 0
   end
 
+  defmodule PartialResult do
+    @moduledoc """
+     Struct used to store partial results during calculations
+    """
+    defstruct x: 0, y: 0, reading: nil
+  end
+
   def calculate_q(readings, started_timestamp) do
-    readings |> Enum.reduce(
-      [],
+    readings |> Enum.reduce([],
       fn reading, acc ->
         acc |> List.insert_at(-1, derivate_point(reading, List.last(acc), started_timestamp))
       end
-    )
+    ) |> Enum.map(fn r -> %Result{x: r.x, y: r.y} end)
   end
 
   defp derivate_point(reading, nil, started_timestamp) do
     y = reading.ph
     x = reading.inserted_at
     d_time = NaiveDateTime.diff(started_timestamp, x)
-    %Result{x: x, y: y/d_time}
+    %PartialResult{x: x, y: y/d_time, reading: reading}
   end
 
   defp derivate_point(reading, last_value, _started_timestamp) do
-    y = reading.ph
+    y = reading.ph - last_value.reading.y
     x = reading.inserted_at
-    d_time = NaiveDateTime.diff(y, last_value.y)
-    %Result{x: x, y: y/d_time}
+    d_time = NaiveDateTime.diff(x, last_value.x)
+    %PartialResult{x: x, y: y/d_time, reading: reading}
   end
 end
